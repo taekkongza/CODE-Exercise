b93e40bc93958955c4a6397113dc474bb39724e2
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-04-22 18:12:22 +0900

    js

---
+++ CodeWithVirag/CWV1_AddToCollectionUI.js
@@ -0,0 +1,14 @@
+// close the section
+
+const closeBtn = document.querySelector('.icon-tabler-letter-x');
+const mainContainer = document.querySelector('.main-container');
+
+
+closeBtn.addEventListener('click', () => {
+  mainContainer.style.transform = "scale(0)";
+});
+
+
+
+
+
