5248aa317155139a685087827af46f9e4c7c89b5
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-04-23 07:31:26 +0900

    111

--- CodeWithVirag/CWV1_AddToCollectionUI.js
+++ CodeWithVirag/CWV1_AddToCollectionUI.js
@@ -18,7 +18,7 @@ const collections = [...document.querySelectorAll('.collection')];
 collections.forEach(collection => {
   collection.addEventListener('click', () => {
     const checkmark = document.createElement('div');
-    checkmark.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>';
+    checkmark.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="#fff"  stroke-width="2.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>';
 
     collection.appendChild(checkmark);
   })
