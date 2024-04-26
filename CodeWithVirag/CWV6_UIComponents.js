f5681225acfa60f304fd37bcb4e9582356d9aeb5
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-04-26 13:30:35 +0900

    zzz

---
+++ CodeWithVirag/CWV6_UIComponents.js
@@ -0,0 +1,9 @@
+// Close the locations section
+const exitBtn = document.querySelector('.icon-tabler-x');
+
+exitBtn.addEventListener('click',()=>{
+  document.querySelector('.location-container').style.display = 'none';
+});
+
+// select a location
+
