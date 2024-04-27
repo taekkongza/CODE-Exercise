83fb3726afa83db4aa7ca55e22eb1c439573e972
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-04-28 00:53:43 +0900

    111

--- CodeWithVirag/CWV7_DetailsCard.js
+++ CodeWithVirag/CWV7_DetailsCard.js
@@ -11,6 +11,46 @@ joinBtn.addEventListener('click', ()=>{
   }   
 });
 
+/*
+textContent
+
+노드의 텍스트 콘텐츠를 가져오거나 설정하는데 사용
+노드의 모든 자식 노드를 포함한 텍스트 콘텐츠를 반환
+*/
+
+/*
+노드란 컴퓨터 과학에서 사용되난 기초 단위
+네트워크나 데이터 구조에서
+정보를 저장하고 처리하는 지점을 의미
+*/
+
+/*
+DOM(document object model)에서의 노드는
+문서의 기본 구성 단위로 
+문서전체, 요소, 속성, 텍스트 등이 
+모두 노드이다.
+예를 들어 <html> <body> <button>등의 태그들은
+모두 노드가 된다.
+*/
+
+
+/*
+classList
+
+요소의 클래스 속성에 대한 접근
+클래스 목록을 추가, 제거, 토글 또는 확인할 수 있음
+
+classList
+    .add( ) 하나 이상의 클래스 추가
+    .remove( ) 하나 이상의 클래스 제거
+    .toggle( ) 클래스가 존재하면 제거하고, 존재하지 않으면 추가
+    .contain( ) 요소가 지정된 클래스를 포함하고 있는지 확인
+    .item( ) 지정된 인덱스의 클래스를 반환
+    .length 클래스 목록의 길이를 반환
+*/
+
+
+
 
 // Create post
 const createPostBtn = document.querySelector('.create-post');
