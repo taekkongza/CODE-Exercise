4d515f9e9538ad0a03146b51fe3afb01fff37d7d
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-06-02 23:34:49 +0900

    1111

---
+++ INSTAGRAM_ParthWebDev/PWD_Dropdowns.js
@@ -0,0 +1,9 @@
+
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+
+dropdownToggle.addEventListener("click", () => {
+  const dropdownMenu = document.querySelector("#dropdown > .menu");
+
+  dropdownMenu.classList.toggle("open");
+  dropdownToggle.classList.toggle("open");
+});
