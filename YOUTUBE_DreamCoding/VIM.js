bd678f553598d12a5f3bdb43f0faecac2feefc24
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-06-17 17:30:04 +0900

    111

---
+++ YOUTUBE_DreamCoding/VIM.js
@@ -0,0 +1,30 @@
+/**
+  vim README.md
+  
+ git commit --amend
+ 
+ 
+ * 명령 모드 ESC , 사각형 커서
+
+ * 명령모드 종료 옵션
+ :q 그냥 종료
+ :q! 강제 종료
+ :w 저장
+ :w! 강제 저장
+ :wq 저장하고 종료 
+ :wq! 강제 저장종료
+
+ 
+ * 입력 모드 i, 'ㅣ' 모양 커서
+ 
+ * intelliJ - ideaVim 설치
+ * VScode - Vim 설치
+ 
+ 
+  
+  
+  
+  
+  
+ */
+
