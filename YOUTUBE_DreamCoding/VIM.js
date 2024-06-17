e811b60f6009c23faa902f2173a25f6a5c03cdcf
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-06-17 18:28:41 +0900

    1111

--- YOUTUBE_DreamCoding/VIM.js
+++ YOUTUBE_DreamCoding/VIM.js
@@ -1,5 +1,5 @@
 /**
-  vim README.md
+ vim README.md
   
  git commit --amend
  
@@ -13,18 +13,60 @@
  :w! 강제 저장
  :wq 저장하고 종료 
  :wq! 강제 저장종료
+ 
+ aaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbccccccccccccccccddddddddddddddd
+ 
+ 방향키
+ H J K L
+ ← ↓ ↑ →
+ 
+ 네비게이션
+0 문장 앞으로 이동
+$ 문장 뒤로 이동
 
- 
- * 입력 모드 i, 'ㅣ' 모양 커서
- 
- * intelliJ - ideaVim 설치
- * VScode - Vim 설치
- 
- 
-  
-  
-  
-  
-  
- */
+w word단어로 이동
+3w 3단어씩 이동
+b backward 단어
 
+H 화면위
+M 화면중간
+L 화면 끝
+
+gg 파일 앞
+G 파일 끝
+
+20G 20번째 줄로 이동
+
+ctrl u 위로 스크롤링
+ctrl d 아래로 스크롤링
+{ 문단의 시작
+} 문단의 끝
+
+
+명령어
+x 커서 아래 글자 삭제
+
+
+
+
+
+* 입력 모드 i, 'ㅣ' 모양 커서
+ ESC 명령모드
+ i insert 커서 앞 입력
+ a append 커서 뒤 입력
+ I 문장 시작 입력 
+ A 문장 끝 입력
+
+
+
+
+
+
+
+* intelliJ - ideaVim 설치 
+* VScode - Vim 설치
+  
+  
+*/
+
+// this is insert mode
