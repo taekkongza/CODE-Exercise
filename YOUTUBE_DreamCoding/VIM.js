902d18f2cc0eaa766bb3042e25c6fd04b6717c02
taekkongza <127930295+taekkongza@users.noreply.github.com>
2024-06-17 22:47:42 +0900

    123123

--- YOUTUBE_DreamCoding/VIM.js
+++ YOUTUBE_DreamCoding/VIM.js
@@ -5,6 +5,7 @@
  
  
  * 명령 모드 ESC , 사각형 커서
+   기본적으로 명령모드로 시작
 
  * 명령모드 종료 옵션
  :q 그냥 종료
@@ -15,7 +16,81 @@
  :wq! 강제 저장종료
  
  aaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbccccccccccccccccddddddddddddddd
- 
+1111111122222222222222222 333333333333333 44444444444444 555555555
+77777777 88888888888888 9999999999999999 000000000000000000000000
+qqqqqqqqqqqqqqqqqqqqqqqqqqq wwwwwwwwwwwwwwwwwwww eeeeeeeeeeeeeee
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRR[what is your destiny?] ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+RRRRRRRuuuuuuur ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+{}rrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrcommand rrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrryyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+rrrrrrrrrrrrrrr ttttttttttttttttttttttt yyyyyyyyyyyyyyyyyyy 
+uuuuuuuuuuuuu iiiiiiiiiiiiiiiiiiii ooooooooooooooooo pppppppppp
+aaaaaaaaaaaa sssssssssss ddddddddddddddd ffffffffffffff gggggggggg
+<p> bcdefghi  </p>
+<p></p>
+<p></p>
+<p> bcdefghi  </p>
+<p> bcdef
+<p> bcdefghi  </p>
+<p> bcdef
+<p> bcdefghi  </p>
+<p> bcdef
+<p> bcdefghi  </p>
+<p> bcdefghi  </p>
+<p> bcdefghi  </p>
+
+
+
+
  방향키
  H J K L
  ← ↓ ↑ →
@@ -45,8 +120,13 @@ ctrl d 아래로 스크롤링
 
 명령어
 x 커서 아래 글자 삭제
+dd 문장 삭제
+yy 문장 복사
+p 붙여넣기
+*p 클립보드 붙여넣기
+setting : vim use system clipboard : check
 
-
+ (개발자라면 한번쯤 꼭 쓴다는 Vim)
 
 
 
@@ -59,14 +139,102 @@ x 커서 아래 글자 삭제
 
 
 
-
-
-
-
 * intelliJ - ideaVim 설치 
 * VScode - Vim 설치
   
-  
+
+
+### Vim 의 강력한 파워
+command + object
+명령 + 무엇에게
+d       3w      
+delete   3 words
+
+d         it
+delete    inner tags
+
+<p> vim is screen </p>
+
+command + object
+명령      무엇에게    3w ,  3b  , aw (a word) , at (a tag), 
+d delete(cut)        ap (a paragraph), as (a sentence)
+y yank(copy)         it (inner tag),   i"(inner "") ip ( inner paragraph)
+c change
+
+명령어
+. 이전 명령어 반복
+u 되감기
+ctrl R 앞
+
+d3j 아래 3줄을 삭제 j는 아래방향키
+
+d3k 위 3줄을 삭제
+di{  중괄호 안을 삭제
+di( 소괄호 안 삭제
+
+da(  소괄호 까지 삭제
+
+di'  ' ' 안 삭제
+da'   '  ' 포함 삭제
+
+ci[   [   ] 내용변경 , 자동으로 입력모드
+
+d 는 잘라내기가 아니라 cut
+daw --> p 붙여넣기
+
+const [show,setShow] = userState(showshow);
+const [show,setShow] = userState(showshow);
+const [show,(showshow);
+const [show,setShow] = userState(showshow);
+const [show,showshow);
+            ____________________
+            이렇게 지우고 싶으면 
+            df(
+            소괄호까지 지워줘
+f 를 이용하라
+            dt(
+            소괄호 앞까지 지워
+t 를 이용하라
+
+어디까지 삭제하는지 보고 싶다. 
+search사용 /
+d/(sh     (sh 앞까지 삭제
+
+d/use   use 앞까지 삭제
+
+
+/ 검색모드
+/찾는 내용
+n 누르면 다음 내용
+N 이전내용
+
+
+? 거꾸로 검색모드
+?찾는내용
+n 누르면 다음내용
+N 이전내용
+
+
+visual select mode
+v
+
+vaw
+
+ctrl+v 블럭단위로 선택
+
+
+
+
+
+
+
+
+
 */
 
 // this is insert mode
+
+
+
+
+
